3cf93d78e8141d9cc19252b4ca9a3dfc0408fa8a
Paolo Scotto <118285718+maciuozz@users.noreply.github.com>
2023-04-02 15:50:18 +0200

    Update WordCounter.java

--- src/main/java/WordCounter/WordCounter.java
+++ src/main/java/WordCounter/WordCounter.java
@@ -3,6 +3,8 @@ package main.java.count;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
