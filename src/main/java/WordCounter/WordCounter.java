3ca3afbf14fbce6f3f5a4650f840f59267a2e84f
Paolo Scotto <118285718+maciuozz@users.noreply.github.com>
2023-04-03 11:35:37 +0200

    Update WordCounter.java

--- src/main/java/WordCounter/WordCounter.java
+++ src/main/java/WordCounter/WordCounter.java
@@ -104,7 +104,7 @@ public class WordCounter {
     //The method searches for fileName in the file system, starting from the root directory. It uses the Files.walk() method to traverse the 
     //directory tree and create a stream of all paths in the file system.
     public static String findFile(String fileName) {
-        Path start = Paths.get("/");
+        Path start = Paths.get(".");
         try (Stream<Path> stream = Files.walk(start)) {
             List<String> paths = stream
                     .filter(path -> Files.isRegularFile(path))
